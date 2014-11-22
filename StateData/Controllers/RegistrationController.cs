43349211fc8cd1d9cd2fa5714387cc3631f20911
yarmolovichalex <yarmolovichalex@gmail.com>
2014-11-22 22:05:51 +0300

    initial commit

---
+++ StateData/Controllers/RegistrationController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StateData.Infrastructure;
+
+namespace StateData.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ProcessFirstForm(string name)
+        {
+            SessionStateHelper.Set(SessionStateKeys.NAME, name);
+            return View("SecondForm");
+        }
+
+        [HttpPost]
+        public ActionResult CompleteForm(string country)
+        {
+            Debug.WriteLine("Country: {0}", (object)country);
+            ViewBag.Name = SessionStateHelper.Get(SessionStateKeys.NAME);
+            ViewBag.Country = country;
+
+            return View();
+        }
+	}
+}
---
+++ StateData/Infrastructure/SessionStateHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StateData.Infrastructure
+{
+    public enum SessionStateKeys
+    {
+        NAME
+    }
+
+    public class SessionStateHelper
+    {
+        public static object Get(SessionStateKeys key)
+        {
+            string keyString = Enum.GetName(typeof (SessionStateKeys), key);
+            return HttpContext.Current.Session[keyString];
+        }
+
+        public static object Set(SessionStateKeys key, object value)
+        {
+            string keyString = Enum.GetName(typeof (SessionStateKeys), key);
+            return HttpContext.Current.Session[keyString] = value;
+        }
+    }
+}
